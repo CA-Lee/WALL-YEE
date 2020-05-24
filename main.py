1f04f71ea353301bf8ad7f167dff5e14fb3ff88e
CA-Lee (Work from home) <iamsamuellee92@gmail.com>
2020-05-25 00:47:51 +0800

    test

--- main.py
+++ main.py
@@ -8,16 +8,31 @@ WEBHOOK = os.environ.get('WEBHOOK')
 bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
 
-bot.set_webhook(url=WEBHOOK)
+# bot.set_webhook(url=WEBHOOK)
 
-@bot.message_handler(commands=['ping'])
-def ping(message):
-        bot.reply_to(message, "PONG~")
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
-@app.route('/', methods=['POST'])
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+@app.route(WEBHOOK, methods=['POST'])
 def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
+
+
+@app.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=WEBHOOK)
+    return "!", 200
+
+
 
 if __name__ == "__main__":
         app.run()

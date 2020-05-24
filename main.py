b259d50441e76591c7ca6c63fab77de4302bbbb7
CA-Lee (Work from home) <iamsamuellee92@gmail.com>
2020-05-25 01:33:26 +0800

    test

--- main.py
+++ main.py
@@ -8,31 +8,25 @@ WEBHOOK = os.environ.get('WEBHOOK')
 bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
 
-#bot.remove_webhook()
-#bot.set_webhook(url=WEBHOOK)
-
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(chat_id=message.chat.id, text='你好，世界！\n\n我的原始碼: [GitHub](https://github.com/CA-Lee/WALL-YEE)')
-
-
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
-
+    bot.send_message(
+        chat_id=message.chat.id,
+        parse_mode="Markdown",
+        disable_web_page_preview=True,
+        text='你好，世界！\n\n我的原始碼: [GitHub](https://github.com/CA-Lee/WALL-YEE)'
+        )
 
 @app.route('/' + TOKEN, methods=['POST'])
 def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
 
-'''
 @app.route("/")
 def webhook():
-    
+    bot.remove_webhook()
     bot.set_webhook(url=WEBHOOK)
     return "!", 200
-'''
 
 if __name__ == "__main__":
     app.run()

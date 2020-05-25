ed956d6899669d7f59f899a1f9cd2e49a5c33f14
CA-Lee (Work from home) <iamsamuellee92@gmail.com>
2020-05-25 20:39:01 +0800

    test

--- main.py
+++ main.py
@@ -28,44 +28,20 @@ def status_addcase(message):
     send_markdown_message(message,str(message.text))
 #####
 '''
-import telegram
-from flask import Flask, request
-from telegram.ext import Dispatcher, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler
 import os
 
 TOKEN = os.environ.get('TOKEN')
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
 
-# Initial Flask app
-app = Flask(__name__)
+def start(update, context):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text='你好，世界！\n我現在什麼都不會！\n\n我的原始碼: [GitHub](https://github.com/CA-Lee/WALL-YEE)'
+        )
 
-# Initial bot by Telegram access token
-bot = telegram.Bot(token=TOKEN)
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
 
-
-@app.route('/' + TOKEN, methods=['POST'])
-def webhook_handler():
-    """Set route /hook with POST method will trigger this method."""
-    if request.method == "POST":
-        update = telegram.Update.de_json(request.get_json(force=True), bot)
-
-        # Update dispatcher process that handler to process this message
-        dispatcher.process_update(update)
-    return 'ok'
-
-
-def reply_handler(bot, update):
-    """Reply message."""
-    text = update.message.text
-    update.message.reply_text(text)
-
-
-# New a dispatcher for bot
-dispatcher = Dispatcher(bot, None)
-
-# Add handler for handling message, there are many kinds of message. For this handler, it particular handle text
-# message.
-dispatcher.add_handler(MessageHandler(Filters.text, reply_handler))
-
-if __name__ == "__main__":
-    # Running server
-    app.run()
+updater.start_webhook(url_path=TOKEN)

7389b7b17823d4e3ed23e33b929a50a2de7666d1
CA-Lee (Work from home) <iamsamuellee92@gmail.com>
2020-05-25 19:14:39 +0800

    test

--- main.py
+++ main.py
@@ -8,14 +8,30 @@ WEBHOOK = os.environ.get('WEBHOOK')
 bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
 
-@bot.message_handler(commands=['start'])
-def start(message):
+def send_markdown_message(message,content):
     bot.send_message(
         chat_id=message.chat.id,
         parse_mode="Markdown",
         disable_web_page_preview=True,
-        text='你好，世界！\n我現在什麼都不會！\n\n我的原始碼: [GitHub](https://github.com/CA-Lee/WALL-YEE)'
+        text=content
+    )
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    send_markdown_message(
+        message,
+        '你好，世界！\n我現在什麼都不會！\n\n我的原始碼: [GitHub](https://github.com/CA-Lee/WALL-YEE)'
         )
+
+#####
+case_list = ['test']
+
+@bot.message_handler(commands=['status_listall'])
+def status_listall(message):
+    reply_text = [(str(case) + '\n') for case in case_list]
+    send_markdown_message(message,reply_text)
+
+#####
 
 @app.route('/' + TOKEN, methods=['POST'])
 def getMessage():

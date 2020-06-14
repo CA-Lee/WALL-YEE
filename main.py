84f9e64b1577a77312e005898ddff6ce4618bb4c
CA-Lee (Work from home) <iamsamuellee92@gmail.com>
2020-06-14 22:39:47 +0800

    fix

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def status_addcase(update, context):
         '成功新增案件，{} 辛苦了❤️'.format(update.message.from_user.full_name),
         quote=False
     )
-    status_listall(bot, update)
+    status_listall(update, context)
 
 @app.route('/' + TOKEN, methods=['POST'])
 def webhook_handler():
